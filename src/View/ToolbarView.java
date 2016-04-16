99203bb8e8452240c43be2eeca16d70fa8151a7b
filibert <arnaud.monteils@isen-lille.fr>
2016-04-16 19:09:45 +0200

    Update toolbar

--- src/View/ToolbarView.java
+++ src/View/ToolbarView.java
@@ -1,17 +1,17 @@
 package view;
 
-import controller.toolbar.FormColoringAction;
-import controller.toolbar.SelectAction;
 import model.Drawing;
-import view.components.RectangleView;
-import view.tools.SelectionToolView;
-import view.tools.ToolView;
+import tools.ToolView;
 
 import javax.swing.*;
 import javax.tools.Tool;
 import java.awt.*;
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ToolbarView{
 
@@ -37,25 +37,6 @@ public class ToolbarView{
             toolBar.add(toolView);
         }
 
-        /*
-        JButton selectButton = toolBar.add(new SelectAction());
-        selectButton.setText("select");
-
-        JButton formColoringButton = toolBar.add(new FormColoringAction());
-        formColoringButton.setText("formColoring");
-
-        toolBar.addSeparator();
-
-        JButton drawOvalButton = toolBar.add(new FormColoringAction());
-        drawOvalButton.setText("ovalButton");
-
-        JButton drawRectangleButton = toolBar.add(new FormColoringAction());
-        drawRectangleButton.setText("rectangle");
-
-        JButton drawLineButton = toolBar.add(new FormColoringAction());
-        drawLineButton.setText("line");
-
-        */
         toolBar.addSeparator();
 
         JPanel jpanel = new JPanel();
@@ -64,7 +45,7 @@ public class ToolbarView{
         JButton ColorSelectButton1 = new JButton();
         JButton ColorSelectButton2 = new JButton();
 
-        ColorSelectButton1.addActionListener(e -> mainColor = JColorChooser.showDialog(null,"Color",null));
+      /*  ColorSelectButton1.addActionListener(e -> mainColor = JColorChooser.showDialog(null,"Color",null));
 
         ColorSelectButton2.addActionListener(e -> secondaryColor = JColorChooser.showDialog(null,"Color",null));
         ColorSelectButton1.setBackground(mainColor);
@@ -72,11 +53,48 @@ public class ToolbarView{
         jpanel.add(ColorSelectButton1);
         jpanel.add(ColorSelectButton2);
 
-        toolBar.add(jpanel);
+        toolBar.add(jpanel);*/
     }
 
     private void populateToolViewList() {
-        toolViewList.add(new SelectionToolView());
+        File toolsFolder = new File(ToolbarView.class.getProtectionDomain().getCodeSource().getLocation().getPath() + "/tools/");
+        File[] fileList = toolsFolder.listFiles();
+        String extension , nameWithExtension , nameWithoutExtension , outerClassName = "";
+
+        Object object;
+        for(File file : fileList){
+            boolean isView = false;
+            nameWithExtension = file.getName();
+            extension = nameWithExtension.substring(nameWithExtension.lastIndexOf("."));
+            nameWithoutExtension = nameWithExtension.substring(0,nameWithExtension.lastIndexOf("."));
+            if (nameWithExtension.contains("$")){
+                outerClassName = nameWithoutExtension.substring(0,nameWithoutExtension.lastIndexOf("$"));
+                if(nameWithoutExtension.endsWith("View")){
+                    isView = true;
+                }
+            }
+            if("ToolView".equals(nameWithoutExtension)|| !isView )
+                continue;
+
+            if(file.isFile() && ".class".equals(extension)){
+                String outerClassNameWithPackage = "tools." + outerClassName;
+                String innerClassNameWithPackage = "tools." + nameWithoutExtension;
+                try {
+                    Class<?> tool = Class.forName(outerClassNameWithPackage);
+                    Object o = tool.newInstance();
+
+                    Class<?> toolView = Class.forName(innerClassNameWithPackage);
+                    Constructor<?> ctor = toolView.getDeclaredConstructor(tool);
+                    Object button =  ctor.newInstance(o);
+                    toolViewList.add((ToolView) button);
+                } catch (ClassNotFoundException | NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | InvocationTargetException e){
+                    e.printStackTrace();
+                }
+
+
+            }
+
+        }
     }
 
     public JToolBar getToolBar() {
